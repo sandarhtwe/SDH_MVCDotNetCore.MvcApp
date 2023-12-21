f9174b88f95b117f0fad3c23bd8865df7d5b89b3
Sandar Htwe <sandarhtwe.sdh16@gmail.com>
2023-12-21 20:40:40 +0630

    Add project files.

---
+++ SDH_MVCDotNetCore.MvcApp/EFDbContext/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SDH_MVCDotNetCore.MvcApp.Models;
+
+namespace SDH_MVCDotNetCore.MvcApp.EFDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
---
+++ SDH_MVCDotNetCore.MvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SDH_MVCDotNetCore.MvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
